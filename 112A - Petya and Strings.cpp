a7ce796980531f9c68b80adfba0daf956bfa0c1e
Mahmudulhasan19 <mahmudulhasanmatin8@gmail.com>
2024-11-22 15:46:30 +0600

    CF

---
+++ 112A - Petya and Strings.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1,s2;
+    cin>>s1>>s2;
+    for(int i=0;i<s1.size();i++)
+    {
+        if(s1[i]<=90)git
+        {
+            s1[i]+=32;
+        }
+        if(s2[i]<=90)
+        {
+            s2[i]+=32;
+        }
+ 
+    }
+    if(s1<s2)
+        cout<<-1;
+    if(s1>s2)
+        cout<<1;
+    if(s1==s2)
+        cout<<0;
+    return 0;
+}
